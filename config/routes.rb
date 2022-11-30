751f0723c2c7f41f063b1d073cfe61ef25e6a427
moringawonder <adams.wonderboy@student.moringaschool.com>
2022-11-30 07:33:12 +0300

    adds routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,18 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  resource :heroes, only: [:index, :show]
+  resource :powers, only: [:index, :show, :update]
+  resource :hero_powers, only: [:index, :update]
+
+  get '/heroes', to: 'heroes#index'
+  get '/heroes/:id', to: 'heroes#show'
+
+  get '/powers/:id', to: 'powers#show'
+  get '/powers', to: 'powers#index'
+  post '/powers/:id', to: 'powers#update'
+
+  get '/hero_powers', to: 'hero_power#index'
+  post '/hero_powers', to: 'hero_power#update'
+
 end
