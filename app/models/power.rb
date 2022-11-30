8151d2225225625cd02a7cfd934f3ee4ea20aac7
moringawonder <adams.wonderboy@student.moringaschool.com>
2022-11-30 07:15:35 +0300

    relationship fix

--- app/models/power.rb
+++ app/models/power.rb
@@ -1,6 +1,6 @@
 class Power < ApplicationRecord
     has_many :hero_powers
-    has_many :heros, through: :hero_powers
+    has_many :heroes, through: :hero_powers
 
     #validation
     validates :description, presence: true, length: { minimum: 20 }
