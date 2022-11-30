a4fc9e1177101196c9c1abf86747b089f66ace73
moringawonder <adams.wonderboy@student.moringaschool.com>
2022-11-30 09:19:46 +0300

    error handling for the models

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,8 @@
 class ApplicationController < ActionController::API
+    # error handling
+    rescue_from ActiveRecord::RecordInvalid, with: :unprocessable_entity_method
+    
+    def unprocessable_entity_method entity
+        render json: {errors: entity.record.errors}, status: :unprocessable_entity
+    end
 end
