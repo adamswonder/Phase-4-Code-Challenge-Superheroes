7eee0c0da1efa4c4120dfa4352b4c17043d5f01d
moringawonder <adams.wonderboy@student.moringaschool.com>
2022-11-30 07:04:45 +0300

    adds validation to heropower model

--- app/models/hero_power.rb
+++ app/models/hero_power.rb
@@ -1,4 +1,7 @@
 class HeroPower < ApplicationRecord
     belongs_to :hero
     belongs_to :power
+
+    # validation
+    validates :strength, inclusion: { in: %w(Strong Weak Average), message: "%{value} is not a valid strength" }
 end
